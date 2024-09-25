a3c254e72d2c2ed73fb974957acf7c6cd2d82dc1
Jonas Quadbeck <jonas.qdbck@gmail.com>
2024-09-25 20:09:21 +0200

    fix build error

--- app/(others)/students/page.tsx
+++ app/(others)/students/page.tsx
@@ -2,11 +2,9 @@
 
 import prisma from "@/utils/prisma";
 import { StudentList } from "./_components/students";
-import { getAccessRights } from "@/utils/accessRights";
 
 export default async function Page() {
   const students = await prisma.student.findMany();
-  const rights = await getAccessRights();
 
-  return <StudentList students={students} rights={rights} />;
+  return <StudentList students={students} />;
 }
