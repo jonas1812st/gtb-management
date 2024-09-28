e3e60da4a11df20ca9bce288756f26f01ec99e81
Jonas Quadbeck <jonas.qdbck@gmail.com>
2024-09-28 15:09:49 +0200

    fixed dayofweek

--- app/(others)/list/page.tsx
+++ app/(others)/list/page.tsx
@@ -5,11 +5,23 @@ import dayjs from "dayjs";
 export default async function Page() {
   const dayOfWeek = dayjs().day();
 
+  const dayOfWeekToAttendanceDay: {
+    [k: number]: number;
+  } = {
+    0: -1,
+    1: 0,
+    2: 1,
+    3: 2,
+    4: 3,
+    5: 4,
+    6: -1,
+  };
+
   const students = await prisma.student.findMany({
     where: {
       attendances: {
         some: {
-          day: dayOfWeek - 1,
+          day: dayOfWeekToAttendanceDay[dayOfWeek],
         },
       },
     },
