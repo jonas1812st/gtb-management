9c50a4cd5ce1b0947661618a60470f59fc6a674d
Jonas Quadbeck <jonas.qdbck@gmail.com>
2024-10-01 16:26:01 +0200

    feat (added zod schema for creating lists)

--- utils/zodSchema.ts
+++ utils/zodSchema.ts
@@ -1,8 +1,12 @@
+import { ListCycleType, ListStudentNotes, TimeManagement } from "@prisma/client";
 import { z } from "zod";
 
+export const TimeSchema = z.number().gte(0).lte(1439).default(960); // 00:00 - 23:59 <=> 0 - 1439
+export const WeekDaySchema = z.number().gte(0).lte(6); // 0 (monday) to 6 (sunday)
+
 export const CreateAttendanceInputSchema = z.object({
-  day: z.number().gte(0).lte(5),
-  end: z.number().gte(0).lte(1439).default(960),
+  day: z.number().gte(0).lte(4),
+  end: TimeSchema,
 });
 
 export const CreateStudentInputSchema = z.object({
@@ -12,4 +16,26 @@ export const CreateStudentInputSchema = z.object({
   grade: z.number(),
   className: z.string().length(1).toUpperCase(),
   attendances: z.array(CreateAttendanceInputSchema.optional()),
+});
+
+export const CreateListActivationSchema = z.object({
+  day: WeekDaySchema,
+  startTime: TimeSchema,
+  startBuffer: z.number().gt(0).lte(60).optional().default(10), // buffer in minutes
+  endTime: TimeSchema,
+  endBuffer: z.number().gt(0).lte(60).optional().default(10), // buffer in minutes
+});
+
+export const CreateListInputSchema = z.object({
+  name: z.string().trim().min(1).max(50),
+  startTime: z.boolean(),
+  endTime: z.boolean(),
+  table: z.object({
+    studentName: z.boolean(),
+    className: z.boolean(),
+    notes: z.nativeEnum(ListStudentNotes),
+  }),
+  cycle: z.nativeEnum(ListCycleType),
+  activations: z.array(CreateListActivationSchema),
+  manageTime: z.nativeEnum(TimeManagement),
 });
