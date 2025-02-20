e2abf5bdc0f2268dd0f1c6c9dfc540ecfcb24149
Jonas Quadbeck <jonas.qdbck@gmail.com>
2025-02-20 18:00:20 +0100

    .

--- components/navigation/topbar.tsx
+++ components/navigation/topbar.tsx
@@ -9,111 +9,79 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { signOut } from "next-auth/react";
 import { PopoverClose } from "@radix-ui/react-popover";
 import { useAccess } from "../cache/accessProvider";
-import { useEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
+import useScrollUp from "@/lib/useScrollUp";
 
 export default function TopBar() {
   const pathname = usePathname();
   const access = useAccess();
 
-  const [show, setShow] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
-
-  const controlNavbar = () => {
-    if (window.scrollY > lastScrollY) {
-      // if scroll down hide the navbar
-      setShow(false);
-    } else {
-      // if scroll up show the navbar
-      setShow(true);
-    }
-
-    // remember current page location to use in the next move
-    setLastScrollY(window.scrollY);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", controlNavbar);
-
-    // cleanup function
-    return () => {
-      window.removeEventListener("scroll", controlNavbar);
-    };
-  }, [lastScrollY]);
+  const show = useScrollUp();
 
   return (
-    <AnimatePresence initial={false}>
-      {show && (
-        <motion.div
-          variants={{ open: { translateY: 0, opacity: 100 }, close: { translateY: "-100%", opacity: 0 } }}
-          initial="close"
-          animate="open"
-          exit="close"
-          transition={{ duration: 0.2, ease: "easeInOut" }}
-          className="sticky top-0 left-0"
-        >
-          <nav className="p-3 shadow-md bg-navigation flex justify-center">
-            <div className="flex justify-between items-center w-[1024px]">
-              <div className="flex items-center space-x-10">
-                <div>
-                  <h1 className="text-navigation-foreground text-2xl font-bold">GTB</h1>
-                </div>
-                <div className="flex space-x-2">
-                  {[
-                    { label: "Aktive Listen", url: "/active" },
-                    { label: "Schüler", url: "/students" },
-                    { label: "Listen", url: "/lists", enabled: access.manageLists },
-                    { label: "Gruppen", url: "/groups", enabled: access.manageGroups },
-                  ]
-                    .filter((element) => element.enabled !== false)
-                    .map((element, index) => (
-                      <Link
-                        href={element.url}
-                        className={cn(
-                          "font-medium py-2 px-3 rounded-md transition duration-100 text-navigation-foreground",
-                          pathname === element.url ? "bg-navigation-active" : "hover:bg-navigation-hover"
-                        )}
-                        key={index + "_top_bar_element"}
-                      >
-                        {element.label}
-                      </Link>
-                    ))}
-                </div>
+    <motion.nav
+      animate={{ translateY: show ? 0 : -100, opacity: show ? 1 : 0 }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+      className="sticky top-0 left-0 z-20 p-3 shadow-md bg-navigation flex justify-center"
+    >
+      <div className="flex justify-between items-center w-[1024px]">
+        <div className="flex items-center space-x-10">
+          <div>
+            <h1 className="text-navigation-foreground text-2xl font-bold">GTB</h1>
+          </div>
+          <div className="flex space-x-2">
+            {[
+              { label: "Aktive Listen", url: "/active" },
+              { label: "Schüler", url: "/students" },
+              { label: "Listen", url: "/lists", enabled: access.manageLists },
+              { label: "Gruppen", url: "/groups", enabled: access.manageGroups },
+            ]
+              .filter((element) => element.enabled !== false)
+              .map((element, index) => (
+                <Link
+                  href={element.url}
+                  className={cn(
+                    "font-medium py-2 px-3 rounded-md transition duration-100 text-navigation-foreground",
+                    pathname === element.url ? "bg-navigation-active" : "hover:bg-navigation-hover"
+                  )}
+                  key={index + "_top_bar_element"}
+                >
+                  {element.label}
+                </Link>
+              ))}
+          </div>
+        </div>
+        <div className="justify-self-end">
+          <Popover>
+            <PopoverTrigger asChild>
+              <button className="text-navigation-foreground flex items-center justify-center rounded-full p-2 bg-navigation-hover transition duration-100 hover:bg-navigation-active">
+                <Icon size={1} path={mdiAccount} />
+              </button>
+            </PopoverTrigger>
+            <PopoverContent align="end" className="p-1.5">
+              <div className="flex flex-col">
+                <PopoverBtn onClick="/users/me">
+                  <Icon size={0.8} path={mdiAccount} />
+                  <span>Profil</span>
+                </PopoverBtn>
+                <PopoverBtn onClick={"/manage"}>
+                  <Icon size={0.8} path={mdiCog} />
+                  <span>Verwaltung</span>
+                </PopoverBtn>
+                <PopoverBtn
+                  onClick={async () => {
+                    await signOut();
+                  }}
+                >
+                  <Icon size={0.8} path={mdiLogout} />
+                  <span>Abmelden</span>
+                </PopoverBtn>
               </div>
-              <div className="justify-self-end">
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <button className="text-navigation-foreground flex items-center justify-center rounded-full p-2 bg-navigation-hover transition duration-100 hover:bg-navigation-active">
-                      <Icon size={1} path={mdiAccount} />
-                    </button>
-                  </PopoverTrigger>
-                  <PopoverContent align="end" className="p-1.5">
-                    <div className="flex flex-col">
-                      <PopoverBtn onClick="/users/me">
-                        <Icon size={0.8} path={mdiAccount} />
-                        <span>Profil</span>
-                      </PopoverBtn>
-                      <PopoverBtn onClick={"/manage"}>
-                        <Icon size={0.8} path={mdiCog} />
-                        <span>Verwaltung</span>
-                      </PopoverBtn>
-                      <PopoverBtn
-                        onClick={async () => {
-                          await signOut();
-                        }}
-                      >
-                        <Icon size={0.8} path={mdiLogout} />
-                        <span>Abmelden</span>
-                      </PopoverBtn>
-                    </div>
-                  </PopoverContent>
-                </Popover>
-              </div>
-            </div>
-          </nav>
-        </motion.div>
-      )}
-    </AnimatePresence>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </motion.nav>
   );
 }
 
---
+++ lib/useScrollUp.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export default function useScrollUp() {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (window.scrollY - lastScrollY > 0) {
+      // if scroll down hide the navbar
+      setShow(false);
+    } else {
+      // if scroll up show the navbar
+      setShow(true);
+    }
+
+    // remember current page location to use in the next move
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+
+    // cleanup function
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  return show;
+}
