fa433c5363577658cc22efbe2f50971094577c27
Jonas Quadbeck <jonas.qdbck@gmail.com>
2025-02-24 14:52:29 +0100

    added better error handling for time notes

--- app/(others)/lists/[id]/[date]/_methods/timeSchema.ts
+++ app/(others)/lists/[id]/[date]/_methods/timeSchema.ts
@@ -15,4 +15,24 @@ export const InputSchema = z
   .refine((values) => values.end === undefined || stringToTime(values.start)! <= stringToTime(values.end)!, {
     message: "End time cannot be lower than start time.",
     path: ["end"],
-  });
+  })
+  .refine(
+    (values) =>
+      (values.end !== undefined && values.endNotes !== undefined) ||
+      (values.end !== undefined && values.endNotes === undefined) ||
+      (values.end === undefined && values.endNotes === undefined),
+    {
+      message: "Specify time before adding end time notes",
+      path: ["end"],
+    }
+  )
+  .refine(
+    (values) =>
+      (values.start !== undefined && values.startNotes !== undefined) ||
+      (values.start !== undefined && values.startNotes === undefined) ||
+      (values.start === undefined && values.startNotes === undefined),
+    {
+      message: "Specify time before adding start time notes",
+      path: ["start"],
+    }
+  );
